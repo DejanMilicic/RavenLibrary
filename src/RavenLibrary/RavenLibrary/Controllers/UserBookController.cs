dc3fc9123e0779f2afa481527afa351ace29e9d5
Dejan Milicic <dejan.milicic@gmail.com>
2020-11-17 00:27:26 +0100

    UserBook model + controller

---
+++ src/RavenLibrary/RavenLibrary/Controllers/UserBookController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RavenLibrary.Models;
+
+namespace RavenLibrary.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserBookController : ControllerBase
+    {
+        [HttpGet]
+        public UserBook Get(string id)
+        {
+            using var session = DocumentStoreHolder.Store.OpenSession();
+            return session.Load<UserBook>(id);
+        }
+    }
+}
---
+++ src/RavenLibrary/RavenLibrary/Models/UserBook.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace RavenLibrary.Models
+{
+    public class UserBook
+    {
+        public string Id { get; set; }
+
+        public string text { get; set; }
+
+        public string book { get; set; }
+
+        public string user { get; set; }
+
+        public int start { get; set; }
+
+        public DateTimeOffset at { get; set; }
+    }
+}
