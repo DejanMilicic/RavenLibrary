09ef9f0b4f2a2b5f0f34eea3ca6de323bcd840ff
Dejan Milicic <dejan.milicic@gmail.com>
2020-11-16 14:51:10 +0100

    Author endpoint

---
+++ src/RavenLibrary/RavenLibrary/Controllers/AuthorController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RavenLibrary.Models;
+
+namespace RavenLibrary.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthorController : ControllerBase
+    {
+        [HttpGet]
+        public Author Get(string id)
+        {
+            using var session = DocumentStoreHolder.Store.OpenSession();
+            return session.Load<Author>(id);
+        }
+    }
+}
---
+++ src/RavenLibrary/RavenLibrary/Models/Author.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace RavenLibrary.Models
+{
+    public class Author
+    {
+        public string Id { get; set; }
+
+        public string name { get; set; }
+
+        public DateTimeOffset created { get; set; }
+
+        public int death_date { get; set; }
+
+        public DateTimeOffset last_modified { get; set; }
+
+        public int latest_revision { get; set; }
+
+        public int birth_date { get; set; }
+
+        public string personal_name { get; set; }
+
+        public RemoteIds remote_ids { get; set; }
+        
+        public int revision { get; set; }
+    }
+
+    public class RemoteIds
+    {
+        public int viaf { get; set; }
+        public string wikidata { get; set; }
+    }
+
+
+}
