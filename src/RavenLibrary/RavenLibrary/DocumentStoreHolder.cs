c030c86f625f97ff39be3281f767c0d9d2460ba8
Dejan Milicic <dejan.milicic@gmail.com>
2020-11-16 14:50:57 +0100

    RavenDB infrastructure

---
+++ src/RavenLibrary/RavenLibrary/DocumentStoreHolder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Raven.Client.Documents;
+
+namespace RavenLibrary
+{
+    public static class DocumentStoreHolder
+    {
+        private static readonly Lazy<IDocumentStore> LazyStore =
+            new Lazy<IDocumentStore>(() =>
+            {
+                var store = new DocumentStore
+                {
+                    Urls = new[] { "http://localhost:8080" },
+                    Database = "Library"
+                };
+
+                return store.Initialize();
+            });
+
+        public static IDocumentStore Store => LazyStore.Value;
+    }
+}
