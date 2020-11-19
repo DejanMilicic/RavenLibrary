cdd792707debb00db9e49458191f72eb15fd1be7
Dejan Milicic <dejan.milicic@gmail.com>
2020-11-19 23:20:56 +0100

    Annotation

---
+++ src/RavenLibrary/RavenLibrary/Models/Annotation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RavenLibrary.Models
+{
+    public class Annotation
+    {
+        public string Id { get; set; }
+
+        public string UserBookId { get; set; }
+
+        public string Text { get; set; }
+
+        public long Start { get; set; }
+
+        public string Note { get; set; }
+
+        public DateTimeOffset Created { get; set; }
+    }
+}
