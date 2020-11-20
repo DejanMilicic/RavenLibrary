f9d87b87ce0c6be23153b8b2b4fb7fd210d978c8
Dejan Milicic <dejan.milicic@gmail.com>
2020-11-20 21:17:53 +0100

    GetUserBooks

--- src/RavenLibrary/RavenLibrary/Controllers/BookController.cs
+++ src/RavenLibrary/RavenLibrary/Controllers/BookController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Raven.Client.Documents;
+using Raven.Client.Documents.Linq;
 using RavenLibrary.Models;
 
 namespace RavenLibrary.Controllers
@@ -21,8 +24,12 @@ namespace RavenLibrary.Controllers
         [HttpGet("/books/user/{userId}/")]
         public async Task<IEnumerable<Book>> GetUserBooks(string userId)
         {
-            // todo implement
-            return new List<Book>();
+            using var session = DocumentStoreHolder.Store.OpenAsyncSession();
+            List<UserBook> userBooks = await session
+                .Query<UserBook>().Include(x => x.BookId).ToListAsync();
+
+            Dictionary<string, Book> books = await session.LoadAsync<Book>(userBooks.Select(x => x.BookId));
+            return books.Values.ToList();
         }
 
         [HttpGet("/books/user/{userId}/{skip}/{take}")]
