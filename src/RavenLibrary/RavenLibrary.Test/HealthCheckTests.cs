441f7416d11b847f4340a6fbad1891f1854c6717
Dejan Milicic <dejan.milicic@gmail.com>
2020-12-01 21:10:18 +0100

    first healthcheck test

---
+++ src/RavenLibrary/RavenLibrary.Test/HealthCheckTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace RavenLibrary.Test
+{
+    public class HealthCheckTests : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly HttpClient _httpClient;
+
+        public HealthCheckTests(WebApplicationFactory<Startup> factory)
+        {
+            _httpClient = factory.CreateDefaultClient();
+        }
+
+        [Fact]
+        public async Task HealthCheck_ReturnsOK()
+        {
+            var response = await _httpClient.GetAsync("/healthcheck");
+
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+    }
+}
