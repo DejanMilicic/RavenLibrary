013d5fd075cbf21c606e808d2c1d8c2496be0d87
Dejan Milicic <dejan.milicic@gmail.com>
2020-12-02 16:51:01 +0100

    sample tests

--- src/RavenLibrary/RavenLibrary.Test/HealthCheckTests.cs
+++ src/RavenLibrary/RavenLibrary.Test/HealthCheckTests.cs
@@ -1,19 +1,38 @@
-﻿using System.Net;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
 using System.Net.Http;
+using System.Net.Http.Json;
 using System.Threading.Tasks;
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using Raven.Client.Documents;
+using Raven.TestDriver;
+using RavenLibrary.Models;
 using Xunit;
 
 namespace RavenLibrary.Test
 {
-    public class HealthCheckTests : IClassFixture<WebApplicationFactory<Startup>>
+    public class HealthCheckTests : RavenTestDriver, IClassFixture<WebApplicationFactory<Startup>>
     {
         private readonly HttpClient _httpClient;
+        private IDocumentStore _store;
 
         public HealthCheckTests(WebApplicationFactory<Startup> factory)
         {
-            _httpClient = factory.CreateDefaultClient();
+            _store = this.GetDocumentStore();
+
+            _httpClient = factory.WithWebHostBuilder(builder =>
+                {
+                    builder.ConfigureTestServices(services =>
+                    {
+                        services.AddSingleton<IDocumentStore>(_store);
+                    });
+                })
+                .CreateDefaultClient();
         }
 
         [Fact]
@@ -22,6 +41,34 @@ namespace RavenLibrary.Test
             var response = await _httpClient.GetAsync("/healthcheck");
 
             response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+
+        [Fact]
+        public async Task GetAnnotations_Returns1()
+        {
+            var session = _store.OpenSession();
+
+            Annotation a = new Annotation
+            {
+                Created = DateTimeOffset.UtcNow,
+                Note = "this is my note",
+                Start = 123,
+                Text = "this is my text about note",
+                UserBookId = "userBooks/123"
+            };
+
+            session.Store(a);
+            session.SaveChanges();
+
+            List<Annotation> dbAnnotations = session.Query<Annotation>().ToList();
+
+            dbAnnotations.Count.Should().Be(1);
+
+            string aid = dbAnnotations.Single().Id;
+
+            var response = await _httpClient.GetFromJsonAsync<Annotation>("/annotation?id=" + aid);
+
+            response.Should().BeEquivalentTo(a);
         }
     }
 }
