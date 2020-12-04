c6ad790632e0cd355555c0f324408e6143285ee3
Dejan Milicic <dejan.milicic@gmail.com>
2020-12-05 00:16:46 +0100

    check fixed

--- src/RavenLibrary/k6/libtests/getuser/smoke.js
+++ src/RavenLibrary/k6/libtests/getuser/smoke.js
@@ -3,7 +3,7 @@ import { check, group, sleep, fail } from 'k6';
 
 export let options = {
   vus: 1, // 1 user looping for 1 minute
-  duration: '1m',
+  duration: '20s',
 
   thresholds: {
     http_req_duration: ['p(99)<1500'], // 99% of requests must complete below 1.5s
@@ -13,8 +13,8 @@ export let options = {
 const BASE_URL = 'https://localhost:5001';
 
 export default () => {
-  let myObjects = http.get(`${BASE_URL}/user?id=users/164496167832`).json();
-  check(myObjects, { 'retrieved user': (obj) => obj.length > 0 });
+  let user = http.get(`${BASE_URL}/user?id=users/164496167832`).json();
+  check(user, { 'user retreived': (u) => u.id == 'users/164496167832' });
 
   sleep(1);
 };
