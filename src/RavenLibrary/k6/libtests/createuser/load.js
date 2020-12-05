56b19c4f9232b01e6d2a25430bacf47e53a1f60e
Dejan Milicic <dejan.milicic@gmail.com>
2020-12-05 23:56:48 +0100

    create user wip

--- src/RavenLibrary/k6/libtests/createuser/load.js
+++ src/RavenLibrary/k6/libtests/createuser/load.js
@@ -6,35 +6,36 @@ import faker from 'https://cdnjs.cloudflare.com/ajax/libs/Faker/3.1.0/faker.min.
 let errorRate = new Rate('error_rate')
 
 export let options = {
-  vus: 1, // 1 user looping for 1 minute
-  duration: '5s',
+  vus: 1, // 1 user looping
+  duration: '20s',
 
   thresholds: {
-    http_req_duration: ['p(99)<1500'], // 99% of requests must complete below 1.5s
+    http_req_duration: ['p(99)<5000'], // 99% of requests must complete below 1.5s
   },
 };
 
 const BASE_URL = 'https://localhost:5001';
 
 const COMMON_REQUEST_HEADERS = {
-  dnt: '1',
-  'user-agent': 'Mozilla/5.0',
-  'content-type': 'application/json',
-  accept: '*/*',
-  origin: BASE_URL,
-  referer: BASE_URL
+  'Accept': 'application/json',
+  'Content-Type': 'application/json',
+  accept: '*/*'
 };
 
 
 export default () => {
-  //let productName = faker.name.findName();
+  const url = 'https://localhost:5001/user';
+  let headers = { 'Content-Type': 'application/json' };
+  let data = { name: faker.name.findName(), karma_comments: 15, karma_links: 66 };
+  let res = http.post(`${BASE_URL}/user`, JSON.stringify(data), { headers: headers });
 
-  let res = http.post(`${BASE_URL}/user`, `{'name': 'john doe', 'karma_Comments':2, 'karma_Links':25}`, {
-    headers: COMMON_REQUEST_HEADERS
-  });
+  headers = { 'Content-Type': 'application/x-www-form-urlencoded' };
+  res = http.post(url, data, { headers: headers });
+
   errorRate.add(res.status >= 400)
 
   check(res, {'status == 200': (r) => r.status === 200 });
+
 
   sleep(1);
 };
