b18accef83cb286a95c32f9c7d9befb5fd905585
Dejan Milicic <dejan.milicic@gmail.com>
2020-12-15 01:33:33 +0100

    (yet to be paged) annotations load test

---
+++ src/RavenLibrary/k6/libtests/scenarios/getPagedAnnotationsForUserBook/load.js
@@ -0,0 +1,49 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+import { Rate } from 'k6/metrics'
+
+let errorRate = new Rate('error_rate')
+
+export let options = {
+  stages: [
+    { duration: '10s', target: 500 }, // simulate ramp-up of traffic
+    { duration: '30s', target: 500 }, // stay at top limit of users
+    { duration: '10s', target: 0 },   // ramp-down to 0 users
+  ],
+
+  summaryTrendStats: ['avg', 'min', 'med', 'max', 'p(90)', 'p(95)', 'p(99)', 'p(99.9)', 'p(99.99)'],
+
+  thresholds: {
+    http_req_duration: ['p(99)<500', 'p(99.9)<1000'],
+  },
+};
+
+let userbooks = JSON.parse(open('./data.json'));
+
+export function setup() {
+  return { userbooks: userbooks }
+}
+
+export default (data) => {
+  let entry = data.userbooks[__VU % data.userbooks.length];
+  let userBookId = entry.id;
+  let annotations = entry.annotations;
+
+  let pageSize = 10;
+  let totalPages = Math.floor(annotations / pageSize) + 1;
+
+  for (let page = 0; page < totalPages; page++) {
+    let skip = page * pageSize
+    
+    //console.log(bookId, annotations, page, totalPages);
+
+    // todo : implement paged endpoint
+    //let res = http.get(`${__ENV.BASE_URL}/annotations/userbook/${skip}/${pageSize}?userId=${userId}`);
+    let res = http.get(`${__ENV.BASE_URL}/annotations/userbook?userBookId=${userBookId}`);
+
+    errorRate.add(res.status >= 400)
+    check(res, {'status == 200': (r) => r.status === 200 });
+  
+    sleep(1);
+  }
+};
