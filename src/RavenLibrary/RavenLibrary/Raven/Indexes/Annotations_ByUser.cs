e8b2c8d931e6f041b6aaa7d50e77929da152d615
Dejan Milicic <dejan.milicic@gmail.com>
2020-12-16 00:48:36 +0100

    Annotations by user

---
+++ src/RavenLibrary/RavenLibrary/Raven/Indexes/Annotations_ByUser.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using Raven.Client.Documents.Indexes;
+using RavenLibrary.Models;
+
+namespace RavenLibrary.Raven.Indexes
+{
+    public class Annotations_ByUser : AbstractIndexCreationTask<Annotation>
+    {
+        public class Result
+        {
+            public string UserId { get; set; }
+        }
+
+        public Annotations_ByUser()
+        {
+            Map = ans => ans
+                .Select(a => new Result
+                {
+                    UserId = a.UserBookId
+                        .Replace("UsersBooks/", "")
+                        .Split(new string[]{ "-" }, StringSplitOptions.RemoveEmptyEntries)[0]
+                });
+        }
+    }
+}
