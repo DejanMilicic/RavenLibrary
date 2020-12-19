e77a6fda27933b53f83c321b251fcf43e8e4a405
Dejan Milicic <dejan.milicic@gmail.com>
2020-12-19 23:51:41 +0100

    map/reduce : books by user

---
+++ src/RavenLibrary/RavenLibrary/Raven/Indexes/Books_ByUser.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using Raven.Client.Documents.Indexes;
+using RavenLibrary.Models;
+
+namespace RavenLibrary.Raven.Indexes
+{
+    public class Books_ByUser : AbstractIndexCreationTask<UserBook, Books_ByUser.Result>
+    {
+        public class Result
+        {
+            public string UserId { get; set; }
+
+            public string BookId { get; set; }
+            
+            public int Count { get; set; }
+        }
+
+        public Books_ByUser()
+        {
+            Map = userBooks => from userBook in userBooks
+                select new
+                {
+                    UserId = userBook.UserId,
+                    BookId = userBook.BookId,
+                    Count = 1
+                };
+
+            Reduce = results => from result in results
+                group result by result.UserId into g
+                select new
+                {
+                    UserId = g.Key,
+                    BookId = "",
+                    Count = g.Sum(x => x.Count)
+                };
+        }
+    }
+}
