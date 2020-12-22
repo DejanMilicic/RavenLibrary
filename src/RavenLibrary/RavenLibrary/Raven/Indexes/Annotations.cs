227e7146aabdb6c601cb079e15015f46fe3fb388
Dejan Milicic <dejan.milicic@gmail.com>
2020-12-22 01:03:53 +0100

    Annotations by date index

---
+++ src/RavenLibrary/RavenLibrary/Raven/Indexes/Annotations.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Raven.Client.Documents.Indexes;
+
+namespace RavenLibrary.Raven.Indexes
+{
+    public class Annotations : AbstractIndexCreationTask
+    {
+        public override string IndexName => "Annotations";
+
+        public override IndexDefinition CreateIndexDefinition()
+        {
+            return new IndexDefinition
+            {
+                Maps =
+                {
+                    @"from u in docs.UserBooks
+select new {
+    u.user,
+    u.at,
+    at_yyyy = u.at.ToString(""yyyy""),
+    at_yyyy_MM = u.at.ToString(""yyyy-MM""),
+    at_yyyy_MM_dd = u.at.ToString(""yyyy-MM-dd"")
+}"
+                }
+            };
+        }
+    }
+}
