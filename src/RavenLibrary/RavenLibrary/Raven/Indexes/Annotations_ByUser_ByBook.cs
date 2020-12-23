bd42ab82bbfc97146b961d1d4e247eed8e37ff2f
Dejan Milicic <dejan.milicic@gmail.com>
2020-12-23 01:35:01 +0100

    Obsolete index, but let's keep it anyway

---
+++ src/RavenLibrary/RavenLibrary/Raven/Indexes/Annotations_ByUser_ByBook.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using Raven.Client.Documents.Indexes;
+using RavenLibrary.Models;
+
+namespace RavenLibrary.Raven.Indexes
+{
+    //public class Annotations_ByUser_ByBook : AbstractIndexCreationTask<Annotation>
+    //{
+    //    public class Result
+    //    {
+    //        public string UserId { get; set; }
+            
+    //        public string BookId { get; set; }
+
+    //        public string AnnotationId { get; set; }
+    //    }
+
+    //    public Annotations_ByUser_ByBook()
+    //    {
+    //        Map = ans => ans
+    //            .Select(a => new Result
+    //            {
+    //                UserId = a.user,
+    //                BookId = a.book,
+    //                AnnotationId = a.Id,
+    //            });
+    //    }
+    //}
+}
