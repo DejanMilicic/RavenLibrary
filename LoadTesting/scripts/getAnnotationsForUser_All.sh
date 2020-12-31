26c63fe3ef5559b8527f3e42d9b32043eaa1e8f6
Dejan Milicic <dejan.milicic@gmail.com>
2020-12-31 16:57:19 +0100

    load testing script and results

---
+++ LoadTesting/scripts/getAnnotationsForUser_All.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+executeLoadTest () {
+  printf "\nLoad test for getAnnotationsForUser_All with $1 concurrent users\n"
+  ../wrk2/wrk -d5m --latency -R $1 -s getAnnotationsForUser_All.lua $2 > getAnnotationsForUser_All_$1.txt
+}
+
+executeLoadTest 250 $1
+executeLoadTest 500 $1
+executeLoadTest 750 $1
