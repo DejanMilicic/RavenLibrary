e9d555853d3161307a824c9a4966f625a4b25dee
Dejan Milicic <dejan.milicic@gmail.com>
2021-01-04 17:43:20 +0100

    scripts folder updated

--- LoadTesting/scripts/getAnnotationsForUserBook_Paged.lua
+++ LoadTesting/scripts/getAnnotationsForUserBook_Paged.lua
@@ -13,12 +13,17 @@ request = function()
 		index = 1
 	end 
 	item = annotations[index]
+	local page = 0
 	local pageSize = 10
-	local reqs = 1
-	for i=1,item.annotations,pageSize do
-		path = "/annotations/userbook/" .. i * pageSize ..  "/" .. pageSize .. "/?userBookId=" .. item.id
-		r[reqs] = wrk.format(nil, path)
-		reqs = reqs + 1
-	end
+	
+	path = "/annotations/userbook/" .. page * pageSize ..  "/" .. pageSize .. "/?userBookId=" .. item.id
+	r[1] = wrk.format(nil, path)
+	
+	-- local reqs = 1
+	--for i=1,item.annotations,pageSize do
+	--	path = "/annotations/userbook/" .. i * pageSize ..  "/" .. pageSize .. "/?userBookId=" .. item.id
+	--	r[reqs] = wrk.format(nil, path)
+	--	reqs = reqs + 1
+	--end
 	return table.concat(r)
 end
--- LoadTesting/scripts/getAnnotationsForUser_All.sh
+++ LoadTesting/scripts/getAnnotationsForUser_All.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-executeLoadTest () {
-  printf "\nLoad test for getAnnotationsForUser_All with $1 concurrent users running for $2 on $3 \n"
-  ../wrk2/wrk -d $2 --latency -R $1 -s getAnnotationsForUser_All.lua $3 > getAnnotationsForUser_All_$1_$2.txt
-}
+for var in "$@"
+do
 
-executeLoadTest 250 $1 $2
-executeLoadTest 500 $1 $2
-executeLoadTest 750 $1 $2
+  executeLoadTest () {
+    printf "\nLoad test for $var with $1 concurrent users running for $2 on $3 \n"
+    ../wrk2/wrk -d $2 --latency -R $1 -s $var $3 > $var_$1_$2.txt
+  }
+
+  executeLoadTest 250 $1 $2
+  executeLoadTest 500 $1 $2
+  executeLoadTest 750 $1 $2
+
+
+done
