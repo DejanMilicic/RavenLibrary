7050f21fedfa08ad167c85591002e213ab8fd965
Dejan Milicic <dejan.milicic@gmail.com>
2021-01-06 12:02:08 +0100

    Load testing project

---
+++ src/RavenLibrary/RavenLibrary.LoadTests/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RavenLibrary.LoadTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
