c1e110a1598d51597f2b8fc37703cc4559cfe5b3
Dejan Milicic <dejan.milicic@gmail.com>
2021-01-08 15:49:19 +0100

    Counchbase client nuget package

---
+++ src/RavenLibrary/CouchLibrary/Controllers/CouchController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CouchLibrary.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CouchController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "cOuch!";
+        }
+    }
+}
