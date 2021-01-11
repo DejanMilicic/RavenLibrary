7e7ac285326ee8d10280f3a0225e94160c033d74
Dejan Milicic <dejan.milicic@gmail.com>
2021-01-11 22:35:53 +0100

    first attempt at Couchbase upsert

--- src/RavenLibrary/CouchLibrary/Controllers/CouchController.cs
+++ src/RavenLibrary/CouchLibrary/Controllers/CouchController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Couchbase;
 
 namespace CouchLibrary.Controllers
 {
@@ -10,6 +12,24 @@ namespace CouchLibrary.Controllers
         public string Get()
         {
             return "cOuch!";
+        }
+
+        [HttpPost]
+        public async Task<string> Create()
+        {
+            var cluster = await Cluster.ConnectAsync("couchbase://localhost", "user", "password");
+
+            // get a bucket reference
+            var bucket = await cluster.BucketAsync("Library");
+
+            // get a collection reference
+            var collection = bucket.DefaultCollection();
+
+            // Upsert Document
+            var upsertResult = await collection.UpsertAsync("my-document-key", new { Name = "Ted", Age = 31 });
+            var getResult = await collection.GetAsync("my-document-key");
+
+            return getResult.ContentAs<dynamic>();
         }
     }
 }
