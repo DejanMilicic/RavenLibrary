83e5895b798c3c244d35d8e010183e768a4ea807
Dejan Milicic <dejan.milicic@gmail.com>
2021-01-20 23:39:33 +0100

    get annotation by id

--- src/RavenLibrary/CouchLibrary/Controllers/CouchAnnotationController.cs
+++ src/RavenLibrary/CouchLibrary/Controllers/CouchAnnotationController.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Couchbase.Linq;
 using CouchLibrary.Models;
+using RavenLibrary.Models;
 
 namespace CouchLibrary.Controllers
 {
@@ -17,6 +19,12 @@ namespace CouchLibrary.Controllers
             _bc = bc;
         }
 
+        [HttpGet("/annotation")]
+        public Annotation Get(string id)
+        {
+            return _bc.Query<Annotation>().FirstOrDefault(x => x.Id == id);
+        }
+
         //[HttpGet("/get/")]
         //public Employee Get(string id)
         //{
@@ -27,7 +35,7 @@ namespace CouchLibrary.Controllers
         //public string Create()
         //{
         //    var employee = new Employee { Id = Guid.NewGuid().ToString(), Name = "Ted", Age = 33 };
-            
+
         //    _bc.Save(employee);
 
         //    return employee.Id;
