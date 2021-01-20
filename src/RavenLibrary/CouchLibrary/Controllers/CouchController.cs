53887ce63853fbe6367838e89804b55614b8d6c2
Dejan Milicic <dejan.milicic@gmail.com>
2021-01-20 23:19:27 +0100

    comment out obsolete code

--- src/RavenLibrary/CouchLibrary/Controllers/CouchController.cs
+++ src/RavenLibrary/CouchLibrary/Controllers/CouchController.cs
@@ -17,20 +17,20 @@ namespace CouchLibrary.Controllers
             _bc = bc;
         }
 
-        [HttpGet("/get/")]
-        public Employee Get(string id)
-        {
-            return _bc.Query<Employee>().FirstOrDefault(x => x.Id == id);
-        }
+        //[HttpGet("/get/")]
+        //public Employee Get(string id)
+        //{
+        //    return _bc.Query<Employee>().FirstOrDefault(x => x.Id == id);
+        //}
 
-        [HttpPost]
-        public string Create()
-        {
-            var employee = new Employee { Id = Guid.NewGuid().ToString(), Name = "Ted", Age = 33 };
+        //[HttpPost]
+        //public string Create()
+        //{
+        //    var employee = new Employee { Id = Guid.NewGuid().ToString(), Name = "Ted", Age = 33 };
             
-            _bc.Save(employee);
+        //    _bc.Save(employee);
 
-            return employee.Id;
-        }
+        //    return employee.Id;
+        //}
     }
 }
